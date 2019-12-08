f6feb433121b037a63d5821f95967561a89b4b6c
unknown <jairo.santos2@fatec.sp.gov.br>
2019-12-08 18:12:26 -0300

    add common oh-my-zsh git aliases. boy, git is verbose.

--- env.bat
+++ env.bat
@@ -1,3 +1,12 @@
 @echo off
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+DOSKEY ga=git add $*
+DOSKEY gd=git diff $*
+DOSKEY gdca=git diff --cached $*
+DOSKEY grh=git reset
+DOSKEY glg=git log $*
+DOSKEY gsb=git status
+DOSKEY gc=git commit $*
+DOSKEY gc!=git commit --amend $*
