dfb33285b4822133e1b4617ced77a4be1db9086f
unknown <jairo.santos2@fatec.sp.gov.br>
2019-12-13 15:29:13 -0300

    subsystem compatible with windows xp; static linking

--- build.bat
+++ build.bat
@@ -8,6 +8,9 @@ cl -DHANDMADE_INTERNAL=1 ^
    -DHANDMADE_WIN32=1 ^
    -nologo -Oi -GR- -EHa- ^
    -W4 -WX -wd4201 -wd4100 -wd4189 ^
-   -Z7 ../win32_handmade.cpp User32.lib Gdi32.lib Kernel32.lib
+   -MT ^
+   -Z7 ../win32_handmade.cpp ^
+   /link -subsystem:windows,5.1 ^
+   User32.lib Gdi32.lib Kernel32.lib
 popd
 
